eb7a3eaae6c1fcb6b143764f84baecb023640be4
Ivan Evtukhovich <evtuhovich@gmail.com>
2013-01-17 13:19:27 +0400

    Fix spec from user macro

--- spec/requests/user_macro_request_spec.rb
+++ spec/requests/user_macro_request_spec.rb
@@ -5,7 +5,7 @@ describe "User Macros" do
   before do
     integration_test
     @host_group = ensure_save(Rubix::HostGroup.new(:name => 'rubix_spec_host_group_1'))
-    @host       = ensure_save(Rubix::Host.new(:name => 'rubix_spec_host_1', :host_groups => [@host_group], :ip => '123.123.123.123'))
+    @host       = ensure_save(Rubix::Host.new(:name => 'rubix_spec_host_1', :host_groups => [@host_group], :interfaces => ['ip' => '123.123.123.123', 'main' => 1]))
   end
 
   after do
@@ -15,7 +15,7 @@ describe "User Macros" do
   describe "when not existing" do
 
     it "returns nil on find" do
-      Rubix::UserMacro.find(:name => 'rubix_spec_macro_1', :host_id => @host.id).should be_nil      
+      Rubix::UserMacro.find(:name => 'rubix_spec_macro_1', :host_id => @host.id).should be_nil
     end
 
     it "can be created" do
@@ -44,6 +44,5 @@ describe "User Macros" do
       @macro.destroy
       Rubix::UserMacro.find(:name => 'rubix_spec_macro_1', :host_id => @host.id).should be_nil
     end
-    
   end
 end
